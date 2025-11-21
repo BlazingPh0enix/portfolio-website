0d9d118ebef28e394a2453fe504c1e2c92ee509d
BlazingPh0enix <anaskazi6789@gmail.com>
2025-11-21 19:47:11 +0530

    feat(scene): add CityGround component and update Scene to include districts and audio effects

--- src/components/Scene/Scene.tsx
+++ src/components/Scene/Scene.tsx
@@ -1,13 +1,18 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Environment } from '@react-three/drei';
+import { Environment } from '@react-three/drei';
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
-import { ExperienceDistrict } from '../Districts/ExperienceDistrict';
-import { useStore } from '../../store/useStore';
+import { DistrictA } from '../Districts/DistrictA';
+import { DistrictB } from '../Districts/DistrictB';
+import { DistrictC } from '../Districts/DistrictC';
+import { CityGround } from './CityGround';
+import { HybridController } from '../Player/HybridController';
+import { CameraRig } from '../Player/CameraRig';
+import { RainAmbience } from '../Audio/RainAmbience';
+import { ThunderEffect } from '../Audio/ThunderEffect';
+import { NavigateOnCommand } from '../Player/NavigateOnCommand';
 
 export const Scene: React.FC = () => {
-  const location = useStore(s => s.currentLocation);
-
   return (
     <Canvas camera={{ position: [10, 8, 12], fov: 55 }} shadows dpr={[1, 1.5]}>
       <color attach="background" args={["#04040a"]} />
@@ -16,8 +21,10 @@ export const Scene: React.FC = () => {
       <ambientLight intensity={0.3} />
       <directionalLight position={[15, 25, 10]} intensity={1.2} castShadow shadow-mapSize-width={1024} shadow-mapSize-height={1024} />
       <Suspense fallback={null}>
-        {/* Single district component currently hosts all placeholder buildings */}
-        <ExperienceDistrict activeLocation={location} />
+        <CityGround />
+        <DistrictA />
+        <DistrictB />
+        <DistrictC />
         {/* Environment HDRI (could be replaced later) */}
         <Environment preset="night" />
         {/* Post-processing effects */}
@@ -25,8 +32,13 @@ export const Scene: React.FC = () => {
           <Bloom intensity={1.2} luminanceThreshold={0.2} luminanceSmoothing={0.9} />
         </EffectComposer>
       </Suspense>
-      {/* Temporary controls for exploration; will be replaced by hybrid controller */}
-      <OrbitControls makeDefault enablePan={false} />
+      {/* Hybrid movement + camera follow */}
+      <HybridController />
+      <CameraRig />
+      <NavigateOnCommand />
+      {/* Ambient weather audio */}
+      <RainAmbience />
+      <ThunderEffect />
     </Canvas>
   );
 };
