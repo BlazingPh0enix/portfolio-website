2a5074baa9c4fd4e067ae81d652e4d0ad2c6dcc9
BlazingPh0enix <anaskazi6789@gmail.com>
2025-11-21 20:04:54 +0530

    feat(app): add mobile joystick support and device detection to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Scene } from './components/Scene/Scene';
 import { Terminal } from './components/UI/Terminal';
 import { Minimap } from './components/UI/Minimap';
 import { LocationLabel } from './components/UI/LocationLabel';
+import { MobileJoystick } from './components/Player/MobileJoystick';
 import { useStore } from './store/useStore';
+import { isMobile } from './utils/DeviceDetector';
 
 export default function App() {
   const isTerminalOpen = useStore(s => s.isTerminalOpen);
+  const [mobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    setMobile(isMobile());
+  }, []);
+
   return (
     <>
       <Scene />
       <LocationLabel />
       <Minimap />
+      {mobile && <MobileJoystick />}
       {isTerminalOpen && <Terminal />}
     </>
   );
