cbe2a31bb731ab2e7b5190b1428b4dfa26fd1af5
BlazingPh0enix <anaskazi6789@gmail.com>
2026-02-15 22:15:12 +0530

    refactor: remove old website design

--- src/utils/DeviceDetector.ts
+++
@@ -1,9 +0,0 @@
-export const isMobile = (): boolean => {
-  if (typeof navigator === 'undefined') return false;
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-};
-
-export const isLowEnd = (): boolean => {
-  if (typeof navigator === 'undefined') return false;
-  return navigator.hardwareConcurrency < 4;
-};
